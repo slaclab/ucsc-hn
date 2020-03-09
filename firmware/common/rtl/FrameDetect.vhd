da89d50c4aa08b0d22946b2e91bb2e07831252fe
vanxiong <vanxiong@slac.stanford.edu>
2020-03-09 14:27:21 -0700

    formatting

--- firmware/common/rtl/FrameDetect.vhd
+++ firmware/common/rtl/FrameDetect.vhd
@@ -16,107 +16,107 @@ library surf;
 use surf.StdRtlPkg.all;
 
 entity FrameDetect is
-	generic(
-		TPD_G			: time		:= 1 ns;
-		REMOVE_SRC_DST_G: sl		:= '0');
-	port(
-		clk         	: in sl;
-		rst				: in sl;
-		din				: in slv(8 downto 0);
-		dinValid		: in sl;
-		dout			: out slv(8 downto 0);
-		doutValid		: out sl);
+   generic(
+      TPD_G            : time := 1 ns;
+      REMOVE_SRC_DST_G : sl   := '0');
+   port(
+      clk       : in  sl;
+      rst       : in  sl;
+      din       : in  slv(8 downto 0);
+      dinValid  : in  sl;
+      dout      : out slv(8 downto 0);
+      doutValid : out sl);
 end entity FrameDetect;
 
 architecture Behavioral of FrameDetect is
 
-	constant packet_start_token_frontend_config	    : slv(7 downto 0) := x"C0"; -- Originates in PC, goes to Frontend
-	constant packet_end_token						: slv(7 downto 0) := x"FF"; -- Signals end of packet
-	
-	type StateType is ( 
-		DETECT_START_S,
-		THROW_AWAY_S,
-		DETECT_END_S);
-		
-	type RegType is record
-		state		: StateType;
-		dout		: slv(7 downto 0);
-		doutValid	: sl;
-		dropByte	: integer range 0 to 3;
-	end record RegType;
-	
-	constant REG_INIT_C : RegType := (
-		state		=> DETECT_START_S,
-		dout		=> (others => '0'),
-		doutValid	=> '0',
-		dropByte	=> 0);
-	
-	signal r	: RegType := REG_INIT_C;
-	signal rin	: RegType;
+   constant packet_start_token_frontend_config : slv(7 downto 0) := x"C0";  -- Originates in PC, goes to Frontend
+   constant packet_end_token                   : slv(7 downto 0) := x"FF";  -- Signals end of packet
+   
+   type StateType is (
+      DETECT_START_S,
+      THROW_AWAY_S,
+      DETECT_END_S);
+
+   type RegType is record
+      state     : StateType;
+      dout      : slv(7 downto 0);
+      doutValid : sl;
+      dropByte  : integer range 0 to 3;
+   end record RegType;
+   
+   constant REG_INIT_C : RegType := (
+      state     => DETECT_START_S,
+      dout      => (others => '0'),
+      doutValid => '0',
+      dropByte  => 0);
+
+   signal r   : RegType := REG_INIT_C;
+   signal rin : RegType;
 
 begin
 
-	comb : process(r, rst, din, dinValid) is
-		variable v : RegType;
-	
-	begin
-		-- Latch the current value
-		v 				:= r;
-		
-		v.doutValid		:= '0';
-		case r.state is
-		    -- Detect and pass through start of frame 0xC0
-			when DETECT_START_S =>
-				v.dout 		:= din;
-				if (din = packet_start_token_frontend_config and dinValid = '1') then
-					v.doutValid	:= '1';
-					if (REMOVE_SRC_DST_G = '1') then
-						v.state := THROW_AWAY_S;
-					else
-						v.state	:= DETECT_END_S;
-					end if;
-				end if;
+   comb : process(r, rst, din, dinValid) is
+      variable v : RegType;
+      
+   begin
+      -- Latch the current value
+      v := r;
 
-			-- Throw away the next two bytes which should be src_id and dst_id;
-			when THROW_AWAY_S =>
-			    if (dinValid = '1') then
-				    v.dropByte := r.dropByte + 1;
-				end if;
-				if (r.dropByte = 2) then
-					v.dropByte 	:= 0;
-					v.state		:= DETECT_END_S;
-				end if;
-			
-			-- Pass through all other data until end of frame of frame 0xFF is detected
-			when DETECT_END_S =>
-				v.dout		:= din;
-				v.doutValid	:= dinValid;
-				if (din = packet_end_token) then
-					v.state	:= DETECT_START_S;
-				end if;
-			
-			when others =>
-			    v.state := DETECT_START_S;
-		end case;
+      v.doutValid := '0';
+      case r.state is
+         -- Detect and pass through start of frame 0xC0
+         when DETECT_START_S =>
+            v.dout := din;
+            if (din = packet_start_token_frontend_config and dinValid = '1') then
+               v.doutValid := '1';
+               if (REMOVE_SRC_DST_G = '1') then
+                  v.state := THROW_AWAY_S;
+               else
+                  v.state := DETECT_END_S;
+               end if;
+            end if;
 
-		-- Synchronous Reset
-		if (rst = '1') then
-			v := REG_INIT_C;
-		end if;
-		
-		-- Register the variable for next clock cycle
-		rin <= v;
-		
-		-- Outputs
-		doutValid <= r.doutValid;
-		dout      <= r.dout;
-	end process comb;
-	
-	seq : process (clk) is
-	begin
-		if (rising_edge(clk)) then
-			r <= rin after TPD_G;
-		end if;
-	end process seq;
+            -- Throw away the next two bytes which should be src_id and dst_id;
+         when THROW_AWAY_S =>
+            if (dinValid = '1') then
+               v.dropByte := r.dropByte + 1;
+            end if;
+            if (r.dropByte = 2) then
+               v.dropByte := 0;
+               v.state    := DETECT_END_S;
+            end if;
+
+            -- Pass through all other data until end of frame of frame 0xFF is detected
+         when DETECT_END_S =>
+            v.dout      := din;
+            v.doutValid := dinValid;
+            if (din = packet_end_token) then
+               v.state := DETECT_START_S;
+            end if;
+            
+         when others =>
+            v.state := DETECT_START_S;
+      end case;
+
+      -- Synchronous Reset
+      if (rst = '1') then
+         v := REG_INIT_C;
+      end if;
+
+      -- Register the variable for next clock cycle
+      rin <= v;
+
+      -- Outputs
+      doutValid <= r.doutValid;
+      dout      <= r.dout;
+   end process comb;
+
+   seq : process (clk) is
+   begin
+      if (rising_edge(clk)) then
+         r <= rin after TPD_G;
+      end if;
+   end process seq;
 end Behavioral;
 
--- firmware/common/rtl/Serializer.vhd
+++ firmware/common/rtl/Serializer.vhd
@@ -17,66 +17,66 @@ use surf.StdRtlPkg.all;
 use surf.AxiStreamPkg.all;
 
 entity Serializer is
-	generic(
-		TPD_G			  : time	  			  := 1 ns;
-		MEMORY_TYPE_G     : string    			  := "distributed";
-		FIFO_ADDR_WIDTH_G : positive  			  := 8;
-		DATA_WIDTH_G      : integer range 5 to 8  := 8);
-	port(
-		clk         	: in sl;
-		rst				: in sl;
-	    tx				: out sl;
-		--AXI
-        mAxisClk        : in  sl;
-        mAxisRst        : in  sl;
-        mAxisMaster     : in AxiStreamMasterType;
-        mAxisSlave      : out  AxiStreamSlaveType);
+   generic(
+      TPD_G             : time                 := 1 ns;
+      MEMORY_TYPE_G     : string               := "distributed";
+      FIFO_ADDR_WIDTH_G : positive             := 8;
+      DATA_WIDTH_G      : integer range 5 to 8 := 8);
+   port(
+      clk         : in  sl;
+      rst         : in  sl;
+      tx          : out sl;
+      --AXI
+      mAxisClk    : in  sl;
+      mAxisRst    : in  sl;
+      mAxisMaster : in  AxiStreamMasterType;
+      mAxisSlave  : out AxiStreamSlaveType);
 end entity Serializer;
 
 architecture Behavioral of Serializer is
 
-	signal dinFrameDetect		: slv(7 downto 0);
-	signal dinFrameDetectValid	: sl;
-	signal doutFrameDetect		: slv(7 downto 0);
-	signal doutFrameDetectValid	: sl;
+   signal dinFrameDetect       : slv(7 downto 0);
+   signal dinFrameDetectValid  : sl;
+   signal doutFrameDetect      : slv(7 downto 0);
+   signal doutFrameDetectValid : sl;
 
-	signal fifoDin				: slv(7 downto 0);
-	signal fifoDinValid			: sl;
-	signal fifoWrEn				: sl;
-	signal fifoDout				: slv(7 downto 0);
-	signal fifoDoutValid		: sl;
+   signal fifoDin       : slv(7 downto 0);
+   signal fifoDinValid  : sl;
+   signal fifoWrEn      : sl;
+   signal fifoDout      : slv(7 downto 0);
+   signal fifoDoutValid : sl;
 
-	signal uartWrData			: slv(7 downto 0);
-	signal uartWrValid			: sl;
-	signal uartWrReady          : sl;
-	signal uartTx				: sl;
+   signal uartWrData  : slv(7 downto 0);
+   signal uartWrValid : sl;
+   signal uartWrReady : sl;
+   signal uartTx      : sl;
 
 begin
 
 ------------------------------------------------------------------------------------------------
 -- Detect start (0xC0) and stop (0xFF) frame. Also remove src and dst id from data.
 ------------------------------------------------------------------------------------------------
-   dinFrameDetect		<= mAxisMaster.tData(7 downto 0);
-   dinFrameDetectValid	<= mAxisMaster.tValid;
+   dinFrameDetect      <= mAxisMaster.tData(7 downto 0);
+   dinFrameDetectValid <= mAxisMaster.tValid;
    U_FrameDetect : entity work.FrameDetect
       generic map (
-         TPD_G             => TPD_G,
-		 REMOVE_SRC_DST_G  => '1')
+         TPD_G            => TPD_G,
+         REMOVE_SRC_DST_G => '1')
       port map (
          -- Clock and Reset
-         clk     	=> clk,
-         rst     	=> rst,
+         clk       => clk,
+         rst       => rst,
          -- Data Interface
-         din	 	=> dinFrameDetect,
-		 dinValid	=> dinFrameDetectValid,
-		 dout		=> doutFrameDetect,
-		 doutValid	=> doutFrameDetectValid);
+         din       => dinFrameDetect,
+         dinValid  => dinFrameDetectValid,
+         dout      => doutFrameDetect,
+         doutValid => doutFrameDetectValid);
 
 ------------------------------------------------------------------------------------------------
 -- UART Tx
 ------------------------------------------------------------------------------------------------
    --Fifo Tx
-   fifoDin  <= doutFrameDetect;
+   fifoDin      <= doutFrameDetect;
    fifoDinValid <= doutFrameDetectValid;
    U_Fifo_Tx : entity surf.Fifo
       generic map (
@@ -88,37 +88,37 @@ begin
          DATA_WIDTH_G    => DATA_WIDTH_G,
          ADDR_WIDTH_G    => FIFO_ADDR_WIDTH_G)
       port map (
-         rst      => rst,  
-         wr_clk   => clk,        
-         wr_en    => fifoDinValid, 
+         rst      => rst,
+         wr_clk   => clk,
+         wr_en    => fifoDinValid,
          din      => fifoDin,
          not_full => fifoWrEn,
-         rd_clk   => clk, 
+         rd_clk   => clk,
          rd_en    => uartWrReady,
          dout     => fifoDout,
          valid    => fifoDoutValid); 
-		 
+
    -- UART TX
-   uartWrData	<= fifoDout;
-   uartWrValid  <= fifoDoutValid;
-   U_UartTx: entity surf.UartTx
+   uartWrData  <= fifoDout;
+   uartWrValid <= fifoDoutValid;
+   U_UartTx : entity surf.UartTx
       generic map (
          TPD_G        => TPD_G,
-		 STOP_BITS_G  => 1,
+         STOP_BITS_G  => 1,
          PARITY_G     => "NONE",
          BAUD_MULT_G  => 4,
          DATA_WIDTH_G => DATA_WIDTH_G)
-	  port map (
-         clk         => clk,
-         rst         => rst,
-         clkEn       => '1',
-         wrData      => uartWrData,
-         wrValid     => uartWrValid,
-         wrReady     => uartWrReady,
-         tx          => uartTx);
+      port map (
+         clk     => clk,
+         rst     => rst,
+         clkEn   => '1',
+         wrData  => uartWrData,
+         wrValid => uartWrValid,
+         wrReady => uartWrReady,
+         tx      => uartTx);
 
-    -- Output
-    mAxisSlave.tReady  <= fifoWrEn;
-	tx				   <= uartTx;
+   -- Output
+   mAxisSlave.tReady <= fifoWrEn;
+   tx                <= uartTx;
 end Behavioral;
 
