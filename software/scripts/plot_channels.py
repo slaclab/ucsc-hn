9285d08467b7fc4eb3fb3b341c1958ad374f3eef
Ryan Herbst <rherbst@slac.stanford.edu>
2020-06-19 10:11:14 -0700

    Update scripts

--- software/scripts/plot_channels.py
+++ software/scripts/plot_channels.py
@@ -4,93 +4,166 @@ import time
 import matplotlib.pyplot as plt
 import matplotlib.backends.backend_pdf
 import sys
-
-if len(sys.argv) != 2:
+from scipy.stats import norm
+import pylab
+if len(sys.argv) < 2:
     print(f"Usage: {sys.argv[0]} data_file.dat")
     sys.exit()
 
-inFile = sys.argv[1]
-outFile = sys.argv[1] + ".pdf"
+print(f"List: {sys.argv[1:]}")
+summary = {}
 
-plots = {}
-
-# Init Data
+# Init summary Data
 for node in range(1):
-    if node not in plots:
-        plots[node] = {}
+    if node not in summary:
+        summary[node] = {}
 
     for board in range(7,10):
-        if board not in plots[node]:
-            plots[node][board] = {}
+        if board not in summary[node]:
+            summary[node][board] = {}
 
         for rena in range(2):
-            if rena not in plots[node][board]:
-                plots[node][board][rena] = {}
+            if rena not in summary[node][board]:
+                summary[node][board][rena] = {}
 
             for channel in range(36):
-                if channel not in plots[node][board][rena]:
-                    plots[node][board][rena][channel] = {'pha' : [], 'pol': 0, 'uVal' : [], 'vVal' : [], 'plt' : None}
+                if channel not in summary[node][board][rena]:
+                    summary[node][board][rena][channel] = {'pol': 0, 'hits': [], 'mean' : [], 'sigma' : []}
 
-print(f"Opening {sys.argv[1]}.")
-print("Reading data......")
-ct = time.time()
-count = 0
+                    for _ in sys.argv[1:]:
+                        summary[node][board][rena][channel]['hits'].append(0)
+                        summary[node][board][rena][channel]['mean'].append(0)
+                        summary[node][board][rena][channel]['sigma'].append(0)
 
-with open(inFile) as f:
-    for line in f:
-        data = line.rstrip().split(' ')
 
-        node   = int(data[0])
-        board  = int(data[1])
-        rena   = int(data[2])
-        chan   = int(data[3])
-        pol    = int(data[4])
-        pha    = int(data[5])
-        uVal   = int(data[6])
-        vVal   = int(data[7])
-        tstamp = int(data[8])
+for sumIdx,inFile in enumerate(sys.argv[1:]):
+    outFile = inFile + ".pdf"
+    print(f"Processing {inFile}")
 
-        plots[node][board][rena][chan]['pol']  = pol
-        plots[node][board][rena][chan]['vVal'].append(vVal)
-        plots[node][board][rena][chan]['uVal'].append(uVal)
-        plots[node][board][rena][chan]['pha'].append(pha)
+    plots = {}
 
-        if int(ct) != int(time.time()):
-            print(f"Read {count} entries")
-            ct = time.time()
+    # Init Data
+    for node in range(1):
+        if node not in plots:
+            plots[node] = {}
 
-        count += 1
+        for board in range(7,10):
+            if board not in plots[node]:
+                plots[node][board] = {}
 
-print("Done reading data")
-print("Generating plots....")
+            for rena in range(2):
+                if rena not in plots[node][board]:
+                    plots[node][board][rena] = {}
 
-pdf = matplotlib.backends.backend_pdf.PdfPages(outFile)
-figs = plt.figure()
+                for channel in range(36):
+                    if channel not in plots[node][board][rena]:
+                        plots[node][board][rena][channel] = {'pha' : [], 'pol': 0, 'uVal' : [], 'vVal' : []}
 
-for node in range(1):
-    for board in range(7,10):
-        for rena in range(2):
+    print("Reading data......")
+    ct = time.time()
+    count = 0
 
-            for page in range(9):
-                fig = plt.figure(figsize=(8.5,11))
+    with open(inFile) as f:
+        for line in f:
+            data = line.rstrip().split(' ')
 
-                for sc in range(4):
-                    channel = page*4 + sc
+            node   = int(data[0])
+            board  = int(data[1])
+            rena   = int(data[2])
+            chan   = int(data[3])
+            pol    = int(data[4])
+            pha    = int(data[5])
+            uVal   = int(data[6])
+            vVal   = int(data[7])
+            tstamp = int(data[8])
 
-                    plt.subplot(2, 2, sc+1)
+            plots[node][board][rena][chan]['pol']  = pol
+            plots[node][board][rena][chan]['vVal'].append(vVal)
+            plots[node][board][rena][chan]['uVal'].append(uVal)
+            if pha != 0:
+                plots[node][board][rena][chan]['pha'].append(pha)
 
-                    _ = plt.hist(plots[node][board][rena][channel]['pha'],bins='auto')
-                    pol = plots[node][board][rena][channel]['pol']
-                    plt.title(f"N{node}, B{board}, R{rena}, C{channel}, P{pol}")
+            if int(ct) != int(time.time()):
+                print(f"Read {count} entries")
+                ct = time.time()
 
-                pdf.savefig(fig)
+            count += 1
 
-pdf.close()
+    print("Done reading data")
+    print("Generating plots....")
 
-print("Done Generating plots")
+    pdf = matplotlib.backends.backend_pdf.PdfPages(outFile)
+    figs = plt.figure()
+    fig = None
+    idx = 0
 
-def plot(node, board, rena, channel):
-    _ = plt.hist(plots[node][board][rena][channel]['pha'],bins='auto')
-    plt.title("Histogram with 'auto' bins")
-    plt.show()
+    for node in range(1):
+        for board in range(7,10):
+            for rena in range(2):
+                for channel in range(36):
 
+                    # Only plot channels with data
+                    if len(plots[node][board][rena][channel]['pha']) != 0:
+                        pol = plots[node][board][rena][channel]['pol']
+                        summary[node][board][rena][channel]['pol'] = pol
+
+                        pha_path = plots[node][board][rena][channel]['pha']
+                        mean_sigma = norm.fit(pha_path)
+                        #print(f"N{node}, B{board}, R{rena}, C{channel}, P{pol} = {mean_sigma} id={id(pha_path)}")
+                        # Fit histogram here
+
+                        summary[node][board][rena][channel]['hits'][sumIdx] = len(plots[node][board][rena][channel]['pha'])
+                        summary[node][board][rena][channel]['mean'][sumIdx] = mean_sigma[0]
+                        summary[node][board][rena][channel]['sigma'][sumIdx] = mean_sigma[1]
+
+                        # Start of a new page
+                        if (idx % 4) == 0:
+                            fig = plt.figure(figsize=(8.5,11))
+
+                        plt.subplot(2, 2, (idx%4)+1)
+
+                        _ = plt.hist(plots[node][board][rena][channel]['pha'],bins='auto')
+                        plt.title(f"N{node}, B{board}, R{rena}, C{channel}, P{pol}")
+
+                        # Last plot of a page
+                        if (idx % 4) == 3:
+                            pdf.savefig(fig)
+                            fig = None
+
+                        idx += 1
+
+    if fig is not None:
+        pdf.savefig(fig)
+
+    pdf.close()
+
+    print("Done Generating plots")
+
+# Save summary data
+with open("summary.csv","w") as f:
+    print("Creating Summary File")
+
+    f.write("node,board,rena,channel,pol")
+
+    for inFile in sys.argv[1:]:
+        f.write(f",{inFile} Hits, {inFile} Mean, {inFile} Sigma")
+
+    f.write("\n");
+
+    # Init summary Data
+    for node in range(1):
+        for board in range(7,10):
+            for rena in range(2):
+                for channel in range(36):
+                    pol = summary[node][board][rena][channel]['pol']
+
+                    f.write(f"{node},{board},{rena},{channel},{pol}")
+
+                    for inIdx,inFile in enumerate(sys.argv[1:]):
+                        f.write(f",{summary[node][board][rena][channel]['hits'][inIdx]}")
+                        f.write(f",{summary[node][board][rena][channel]['mean'][inIdx]:.2f}")
+                        f.write(f",{summary[node][board][rena][channel]['sigma'][inIdx]:.2f}")
+
+                    f.write("\n");
+
+    print("Done")
---
+++ software/scripts/thold_scan.py
@@ -0,0 +1,74 @@
+import pyrogue.interfaces
+import time
+
+client = pyrogue.interfaces.VirtualClient('localhost',9099)
+
+print(f"Time= {client.MultiRenaRoot.LocalTime.get()}")
+
+# Take state steps as a script
+client.MultiRenaRoot.LoadConfig("/home/ryan/work/ucsc-hn/software/scripts/defaults_scan.yml")
+print("Load Done")
+
+time.sleep(5)
+client.MultiRenaRoot.Initialize()
+print("Initialize Done")
+
+Duration = 60
+DataDir = "thold_single1"
+
+#TholdValues = [180, 178, 176, 174, 172]
+#ChanList = [i for i in range(25,29)]
+
+TholdValues = [50, 44, 46, 48, 50,52,54,56,58,60,62,64]
+#ChanList = [i for i in range(4,25)]
+ChanList = [i for i in range(4,7)]
+
+for chan in ChanList:
+
+    # Update thresholds
+    for thold in TholdValues:
+        for b in range(7,9):
+            for r in range(2):
+                client.MultiRenaRoot.Node[0].RenaArray.RenaBoard[b].Rena[r].Channel[chan].FastDac.set(thold)
+                client.MultiRenaRoot.Node[0].RenaArray.RenaBoard[b].Rena[r].Channel[chan].SlowDac.set(thold)
+
+                # Turn off other channels
+                for other in range(0,36):
+                    client.MultiRenaRoot.Node[0].RenaArray.RenaBoard[b].Rena[r].Channel[other].SlowTrigEnable.setDisp('Enable')
+                    client.MultiRenaRoot.Node[0].RenaArray.RenaBoard[b].Rena[r].Channel[other].SlowTrigEnable.setDisp('Enable')
+
+                # Turn on target channels
+                client.MultiRenaRoot.Node[0].RenaArray.RenaBoard[b].Rena[r].Channel[chan].SlowTrigEnable.setDisp('Enable')
+                client.MultiRenaRoot.Node[0].RenaArray.RenaBoard[b].Rena[r].Channel[chan].SlowTrigEnable.setDisp('Enable')
+
+    # Configure boards
+    client.MultiRenaRoot.Node[0].RenaArray.ConfigBoards()
+    time.sleep(2)
+    client.MultiRenaRoot.Initialize()
+    time.sleep(1)
+
+    # Open data file
+    client.MultiRenaRoot.DataWriter.DataFile.set(f"/home/ryan/work/ucsc-hn/software/scripts/{DataDir}/{chan:#02}_{thold:#03}.dat")
+    client.MultiRenaRoot.DataWriter.Open()
+    time.sleep(1)
+
+    # Set run enable
+    client.MultiRenaRoot.RunControl.runState.setDisp("Running")
+
+    print(f"Running for {Duration} seconds Chan {chan} Thold {thold}")
+    time.sleep(Duration)
+
+    # Set run stopped
+    client.MultiRenaRoot.RunControl.runState.setDisp("Stopped")
+    time.sleep(1)
+
+    # Close data file
+    client.MultiRenaRoot.DataWriter.Close()
+
+print("Done")
+
+
+# Changing dac values
+#client.MultiRenaRoot.Node[0].RenaArray.RenaBoard[7].Rena[0].Channel[0].FastDac.set(100)
+#client.MultiRenaRoot.Node[0].RenaArray.RenaBoard[7].Rena[0].Channel[0].SlowDac.set(100)
+
