68d9c5e9e039c76e506db353009b492a87ed0e81
Ryan Herbst <rherbst@slac.stanford.edu>
2023-01-23 18:20:06 -0800

    Update impl settings

--- firmware/targets/SlaveRena/ruckus.tcl
+++ firmware/targets/SlaveRena/ruckus.tcl
@@ -1,6 +1,9 @@
 # Load RUCKUS environment and library
 source -quiet $::env(RUCKUS_DIR)/vivado_proc.tcl
 
+# Updating the impl_1 strategy
+set_property strategy Performance_ExplorePostRoutePhysOpt [get_runs impl_1]
+
 # Load common and sub-module ruckus.tcl files
 loadRuckusTcl $::env(PROJ_DIR)/../../submodules/surf
 loadRuckusTcl $::env(PROJ_DIR)/../../submodules/rce-gen3-fw-lib/RceG3
