7b9b99dd9bb5eae7a7ec17f5fcc95474e8c1edee
Ryan Herbst <rherbst@slac.stanford.edu>
2023-03-10 13:04:17 -0800

    Update strategy

--- firmware/targets/MultiRena/ruckus.tcl
+++ firmware/targets/MultiRena/ruckus.tcl
@@ -1,6 +1,8 @@
 # Load RUCKUS environment and library
 source -quiet $::env(RUCKUS_DIR)/vivado_proc.tcl
 
+set_property strategy Performance_ExplorePostRoutePhysOpt [get_runs impl_1]
+
 # Load common and sub-module ruckus.tcl files
 loadRuckusTcl $::env(PROJ_DIR)/../../submodules/surf
 loadRuckusTcl $::env(PROJ_DIR)/../../submodules/rce-gen3-fw-lib/RceG3
